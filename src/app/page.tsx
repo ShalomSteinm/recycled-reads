a27fbcec699d3ea9b8a429147f7a5c4dd2ea29bf
Sholom <boruchsteinmetz@outlook.com>
2023-09-05 10:46:09 -0400

    style table for showing book information

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   const books = [
     {
       id: 1,
-      name: "Book 1",
+      title: "Book 1",
       author: "Author A",
       condition: "Good",
       price: "$15",
@@ -14,46 +14,77 @@ export default function Home() {
     },
     {
       id: 2,
-      name: "Book 2",
+      title: "Book 2",
       author: "Author B",
       condition: "Like New",
       price: "$20",
       seller: "Seller 2",
       url: "/handmaid.jpg",
     },
-    // Add more books...
   ];
   return (
     <>
-      <h1>Recycled Reads</h1>
-      <h3>Every page has a New Adventure</h3>
+      <h1 style={{ marginBottom: "0px" }}>Recycled Reads</h1>
+      <h3 style={{ marginTop: "0.25rem" }}>Online Used Book Marketplace</h3>
+      <header style={{ textAlign: "center", marginTop: "2rem" }}>
+        Top 100 Bestselling Books
+      </header>
 
-      <table id={styles["book-list"]}>
-        <tr>
-          <th>Name</th>
-          <th>Author</th>
-          <th>Condition</th>
-          <th>price</th>
-          <th>Seller</th>
-        </tr>
+      <section className={styles["book-list"]}>
         {books.map((book) => (
-          <tr key={book.id}>
-            <div>
-              <Image
-                width="50"
-                height="50"
-                src={book.url}
-                alt="handsmaid tale book cover"
-              ></Image>
+          <div className={styles["book"]} key={book.id}>
+            <Image
+              width="160"
+              height="220"
+              src={book.url}
+              alt="handsmaid tale book cover"
+            ></Image>
+            <div className={styles["product-info-item"]}>
+              <h3 style={{ margin: "0px", lineHeight: "1rem" }}>
+                {book.title}
+              </h3>
+              <div
+                style={{
+                  fontSize: "1rem",
+                  marginBottom: "1rem",
+                  marginTop: "0.2rem",
+                }}
+              >
+                by <span>{book.author}</span>
+              </div>
+
+              <table>
+                <thead>
+                  <tr>
+                    <th>Condition</th>
+                    <th>Price</th>
+                    <th>Seller</th>
+                  </tr>
+                </thead>
+
+                <tbody>
+                  <tr>
+                    <td>{book.condition}</td>
+                    <td>{book.price}</td>
+                    <td>{book.seller}</td>
+                  </tr>
+                </tbody>
+              </table>
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "end",
+                  marginTop: "6.2rem",
+                }}
+              >
+                <button className={styles.cartButton} type="button">
+                  Add To Cart
+                </button>
+              </div>
             </div>
-            <td>{book.name}</td>
-            <td>{book.author}</td>
-            <td>{book.condition}</td>
-            <td>{book.price}</td>
-            <td>{book.seller}</td>
-          </tr>
+          </div>
         ))}
-      </table>
+      </section>
     </>
   );
 }
